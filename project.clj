da7b31787fddf39d3e5279bb3bb184f8f455f415
Anton Podviaznikov <podviaznikov@gmail.com>
2013-10-04 17:05:35 -0400

    add travis file. update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ls "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "One-method library. Get all files in the given directory. Similar to UNIX `ls`."
+  :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
+  :url "https://github.com/hashobject/sitemap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
